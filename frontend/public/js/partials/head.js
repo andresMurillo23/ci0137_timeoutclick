28548328a78ae54a58fd9abcd13c6d85e89d6e37
Rolando Villavicencio <jose.villavicenciogonzalez@ucr.ac.cr>
2025-10-17 00:36:11 -0600

    feat: remove unused template files and add navbar, head, and footer templates
    Co-authored-by: tatsparamo <gipsyparamolopez@gmail.com>
    Co-authored-by: andresMurillo23 <andresmimi@gmail.com>
    Co-authored-by: isabellarodriguezs <isabella.rodriguezsanchez@ucr.ac.cr>

---
+++ frontend/public/js/partials/head.js
@@ -0,0 +1,12 @@
+var head =
+`<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="/css/app.css" />
+</head>
+<body>
+  <div class="app">
+`;
+document.write(head);
