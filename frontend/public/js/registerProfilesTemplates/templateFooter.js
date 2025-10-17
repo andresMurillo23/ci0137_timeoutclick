179800c9c340337271de95fc9b41f44f4a4fc1ea
GYPSI.PARAMO <gipsyparamolopez@gmail.com>
2025-10-16 18:09:45 -0600

    feat: add footer template with back button

---
+++ frontend/public/js/registerProfilesTemplates/templateFooter.js
@@ -0,0 +1,9 @@
+var templateFooter = `<div class="footer">
+        <button class="btn" type="button">Back</button>
+
+      </div>
+    </main>
+  </div>
+</body>
+</html>`;
+document.write(templateFooter);
